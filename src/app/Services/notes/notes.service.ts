76bb170cafd071644976a66671b63da4d37fb4d4
Aakash Vishwakarma <aakashvishu120@gmail.com>
2025-07-14 12:21:19 +0530

    [Aakash] Refactor : Rafactor code of notes service header

--- src/app/Services/notes/notes.service.ts
+++ src/app/Services/notes/notes.service.ts
@@ -9,94 +9,36 @@ export class NotesService {
 
   constructor(private http: HttpService) { }
 
-  addNotes(data: any) {
-    const token = localStorage.getItem('Token');
-    console.log("mytoken inside addNotes", token);
-    const headers = new HttpHeaders({
-      "Authorization": token || ''
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('Token') || '';
+    return new HttpHeaders({
+      'Authorization': token,
+      'Content-Type': 'application/json'
     });
-
-    return this.http.postApi(`/notes/addNotes`, data, headers);
   }
 
-
-  // getNotes(){
-  // const token = localStorage.getItem('Token');
-  //   console.log("mytoken inside getNotes", token);
-  //   const headers = new HttpHeaders({
-  //     "Authorization" : token || ''
-  //   });
-  //   const queryParams = `?access_token=${token}`;
-  //   // return this.http.getApi(`/notes/getNotesList${queryParams}`);
-  //   return this.http.getApi(`/notes/getNotesList`, {headers:headers});
-  //   // return this.http.getApi(`/notes/addNotes`, headers);
-
-  // }
-
+  addNotes(data: any) {
+    return this.http.postApi(`/notes/addNotes`, data, this.getAuthHeaders());
+  }
 
   getNotes() {
-    const token = localStorage.getItem('Token');
-    let httpOption = {
-      headers: new HttpHeaders(
-        {
-          'Authorization': `${token}`,
-          'Content-Type': 'application/json'
-        })
-    };
-    // console.log('Headers:', httpOption);
-    return this.http.getApi('/notes/getNotesList', httpOption.headers);
+    return this.http.getApi('/notes/getNotesList', this.getAuthHeaders());
   }
 
   getTrashNotesList() {
-    const token = localStorage.getItem('Token');
-    let httpOption = {
-      headers: new HttpHeaders(
-        {
-          'Authorization': `${token}`,
-          'Content-Type': 'application/json'
-        })
-    };
-    // console.log('Headers:', httpOption);
-    return this.http.getApi('/notes/getTrashNotesList', httpOption.headers);
+    return this.http.getApi('/notes/getTrashNotesList', this.getAuthHeaders());
   }
 
-  trashNotes(data:any) {
-    const token = localStorage.getItem('Token');
-    let httpOption = {
-      headers: new HttpHeaders(
-        {
-          'Authorization': `${token}`,
-          'Content-Type': 'application/json'
-        })
-    };
-    return this.http.postApi('/notes/trashNotes', data , httpOption.headers);
+  trashNotes(data: any) {
+    return this.http.postApi('/notes/trashNotes', data, this.getAuthHeaders());
   }
 
-    getArchiveNotesList(){
-    const token = localStorage.getItem('Token');
-    let httpOption = {
-      headers: new HttpHeaders(
-        {
-          'Authorization': `${token}`,
-          'Content-Type': 'application/json'
-        })
-    };
-    // console.log('Headers:', httpOption);
-    return this.http.getApi('/notes/getArchiveNotesList', httpOption.headers);
+  getArchiveNotesList() {
+    return this.http.getApi('/notes/getArchiveNotesList', this.getAuthHeaders());
   }
 
-
-  archiveNotes(data:any) {
-    console.log("reached inside notes service")
-    const token = localStorage.getItem('Token');
-    let httpOption = {
-      headers: new HttpHeaders(
-        {
-          'Authorization': `${token}`,
-          'Content-Type': 'application/json'
-        })
-    };
-    return this.http.postApi('/notes/archiveNotes', data , httpOption.headers);
+  archiveNotes(data: any) {
+    return this.http.postApi('/notes/archiveNotes', data, this.getAuthHeaders());
   }
 
 }
